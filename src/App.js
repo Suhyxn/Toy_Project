96bc7b2e70c03bcdbe3470383040f747aabefca1
Suhyxn <momentomori21489@gmail.com>
2022-08-18 16:07:20 +0900

    Footer Foundation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <span to="/post">Post</span> |
         <span to="/register">Register</span>
       </>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { IoIosPaw } from 'react-icons/io'
+
+function Footer() {
+  return (
+    <div>
+      <div>Footer</div>
+      <div>
+        < IoIosPaw />
+        내 지역 보기
+      </div>
+      <div>
+        < IoIosPaw />
+        전체 지도 보기
+      </div>
+      <div>
+        < IoIosPaw />
+        즐겨찾기
+      </div>
+    </div>
+  )
+}
+
+export default Footer
