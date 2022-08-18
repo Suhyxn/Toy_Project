86e1e3726e0f6a737246a571243398b2abddec4f
Suhyxn <momentomori21489@gmail.com>
2022-08-18 15:28:17 +0900

    Header Foundation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
-      <>
-      <select>
-        <option>1</option>
-        <option>2</option>
-      </select>
-      <select>
-        <option>3</option>
-        <option>4</option>
-      </select>
-      </>
+      <Header />
       <>
         <div>content
           <div>title</div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+function Header() {
+  const [data, setData] = useState(null)
+
+  const getData = async () => {
+    const response = await axios.get('http://localhost:8000/response')
+    console.log(response.data)
+    setData(response.data['body']['items'])
+  }
+
+  useEffect(()=> {
+    getData()
+  }, [])
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  return (
+    <div>
+      <div>Header</div>
+      <select name="sidoName">
+        <option value="서울">서울</option>
+      </select>
+      <select>
+        {data && data.map((d)=> (
+          <option key={d.stationName}>{d.stationName}</option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export default Header
