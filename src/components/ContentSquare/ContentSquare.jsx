ceea16b649a65ab36a875698f050dfceaa45f216
Suhyxn <momentomori21489@gmail.com>
2022-08-21 04:24:21 +0900

    Feat: ContentSquare Style Foundation

--- src/components/ContentSquare/ContentSquare.jsx
+++ src/components/ContentSquare/ContentSquare.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import * as S from './style'
 
 function ContentSquare() {
   const [data, setData] = useState(null)
+
+  const [sidoName, setSidoName] = useState(null)
 
   const getParameters = {
     serviceKey:'hRL8jDNgntV6amiFWZQPeHEqRu1mbof%2FJP%2BIoqhYt0g7Qs0UtrwOAWjpBy6BThiIK%2FeiTslekA3BQ%2BujQhXHXg%3D%3D',
@@ -23,6 +26,8 @@ function ContentSquare() {
     const response = await axios.get('http://localhost:8000/response',{params: getParameters})
     console.log(response.data)
     setData(response.data['body']['items'])
+    setSidoName(response.data['body']['items']['sidoName'])
+    console.log(sidoName)
   }
 
   useEffect(()=> {
@@ -36,17 +41,21 @@ function ContentSquare() {
   return (
     <div>
       <div>Content</div>
-      <div>
+      <S.Container>
         {data && data.map((data)=> (
-          <>
-          <p>{data.stationName}</p>
-          <p>{data.sidoName}</p>
-          <p>{data.pm10Grade}</p>
-          <p>{data.pm10Value}</p>
-          <p>{data.dataTime}</p>
-          </>
+          <S.ContentBox>
+            <S.ContentTitle>
+              <S.StationName>{data.stationName}</S.StationName>
+              <S.SidoName>{data.sidoName}</S.SidoName>
+            </S.ContentTitle>
+            <S.ContentIn>
+              <S.Pm10Grade>{data.pm10Grade}</S.Pm10Grade>
+              <S.Pm10Value>미세먼지 수치 : {data.pm10Value}</S.Pm10Value>
+              <S.DataTime>({data.dataTime} 기준)</S.DataTime>
+            </S.ContentIn>
+          </S.ContentBox>
         ))}
-      </div>
+      </S.Container>
     </div>
   )
 }
--- src/components/ContentSquare/style.js
+++ src/components/ContentSquare/style.js
@@ -1,1 +1,63 @@
 import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const ContentBox = styled.div`
+  width: 350px;
+  height: 200px;
+  background-color: skyblue;
+  border-radius: 10%;
+  margin-top: 50px;
+`
+
+export const ContentTitle = styled.div`
+  margin-left: 14px;
+  margin-top: 14px;
+`
+
+export const StationName = styled.span`
+  color: white;
+  font-weight: bold;
+  font-size: 25px;
+`
+
+export const SidoName = styled.span`
+  color: white;
+  margin-left: 12px;
+  font-size: 15px;
+  font-weight: 600;
+`
+
+export const ContentIn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const Pm10Grade = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  margin-top: 10px;
+  width: 240px;
+  height: 80px;
+  border-radius: 10%;
+  color: skyblue;
+  font-size: 20px;
+`
+
+export const Pm10Value = styled.div`
+  margin-top: 10px;
+  color: white;
+`
+
+export const DataTime = styled.div`
+  color: white;
+`
